8df7392753059499a2ad504b66755a6be78b2cf1
PaulMahar <paulmahar@outlook.com>
2025-03-19 14:02:28 -0300

    Add project files.

---
+++ TicketApi/Controllers/TicketController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TicketApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TicketController : ControllerBase
+    {
+
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly IConfiguration _configuration;
+
+        public TicketController(ILogger<WeatherForecastController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello from Ticket Controller - GET()");
+        }
+
+        [HttpPost]
+        public IActionResult Post()
+        {
+            return Ok("Hello from the Ticket Controller - POST()");
+        }
+    }
+}
