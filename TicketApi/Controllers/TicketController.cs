8c4add8aded2f4e72dd486d8aaf9bb07644897dc
PaulMahar <paulmahar@outlook.com>
2025-03-26 13:32:29 -0300

    Clean up

--- TicketApi/Controllers/TicketController.cs
+++ TicketApi/Controllers/TicketController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Azure.Storage.Queues;
+using System.Runtime.InteropServices.Marshalling;
+using System.Text.Json;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace TicketApi.Controllers
@@ -26,14 +29,28 @@ namespace TicketApi.Controllers
         }
 
         [HttpPost]
-        public IActionResult Post(Contact contact)
+        public async Task<IActionResult> Post(Contact contact)
         {
 
             if (ModelState.IsValid == false)
             {
                 BadRequest(ModelState);
             }
+
+            string queueName = "tickets";
+            string connectionString = "DefaultEndpointsProtocol=https;AccountName=nscc0179231storageacct;AccountKey=x4h6WI7XR4SFBPht5GsZI/+FoBtP9Urwj8r9me6NZyTMpt7j4cuAS/Yviyy5DDaFl37q7WSI2uHF+AStV40qEQ==;EndpointSuffix=core.windows.net";
+            QueueClient queueClient = new QueueClient(connectionString, queueName);
+
+            // serialize an object to json
+            string message = JsonSerializer.Serialize(contact);
+
+            // send string message to queue
+            await queueClient.SendMessageAsync(message);
+
             return Ok(contact);
         }
+
+
     }
 }
+
