357cf7b761253657bcc99fa9a41e8580d4472222
PaulMahar <paulmahar@outlook.com>
2025-03-29 11:47:52 -0300

    Final Push

--- TicketApi/Contact.cs
+++ TicketApi/Contact.cs
@@ -4,20 +4,21 @@ namespace TicketApi
 {
     public class Contact
     {
+        [Required]
         public int concertId { get; set; }
 
 
         [EmailAddress(ErrorMessage = "Please enter a valid email address (name@example.com)")]
         [Required(ErrorMessage = "An email address is required to proceed.")]
         public String Email { get; set; } = string.Empty;
+        
+
+        [Required(ErrorMessage = "Name is required.")]
+        public String Name { get; set; } = string.Empty;
 
 
-        [Required(ErrorMessage = "First name is required.")]
-        public String FirstName { get; set; } = string.Empty;
-
-
-        [Required(ErrorMessage = "Last name is required.")]
-        public String LastName { get; set; } = string.Empty;
+        //[Required(ErrorMessage = "Last name is required.")]
+        //public String LastName { get; set; } = string.Empty;
 
 
         [Required(ErrorMessage = "Please enter a phone number.")]
@@ -44,17 +45,21 @@ namespace TicketApi
         [Required(ErrorMessage = "Security Code is required.")]
         [RegularExpression(@"^\d{3,4}$", ErrorMessage = "Security Code must be 3 or 4 digits.")]
         public String SecurityCode {  get; set; } = String.Empty;
-        
+
+        [Required]
         public String Address { get; set; } = String.Empty;
 
+        [Required]
         public String City { get; set; } = String.Empty;
 
+        [Required]
         public String Province { get; set; } = String.Empty;
 
         [Required(ErrorMessage = "Postal Code is required.")] //I got this regex from the internet, found a great resource at https://regex101.com/
         [RegularExpression(@"(^[A-Za-z]\d[A-Za-z][ -]?\d[A-Za-z]\d$)|(^\d{5}(-\d{4})?$)", ErrorMessage = "Postal Code must be a valid Canadian or US postal code.")]
         public String PostalCode { get; set; } = String.Empty;
 
+        [Required]
         public String Country { get; set; } = String.Empty;
 
     }
